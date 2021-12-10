133fe55a146aef787dc34af6a89ca4c51df07d7a
Venuja104 <95410898+Venuja104@users.noreply.github.com>
2021-12-10 16:31:49 +0530

    Update gettime.py

--- Alpha/modules/gettime.py
+++ Alpha/modules/gettime.py
@@ -2,8 +2,8 @@ import datetime
 from typing import List
 
 import requests
-from Shizu import TIME_API_KEY, dispatcher
-from Shizu.modules.disable import DisableAbleCommandHandler
+from Alpha import TIME_API_KEY, dispatcher
+from Alpha.modules.disable import DisableAbleCommandHandler
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, run_async
 
