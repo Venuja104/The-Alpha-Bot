097eb1fba60df5884fdb437e927533fb99ec7855
TeamShizu <rasindu.gimhan.21@gmail.com>
2021-12-10 10:44:16 +0000

    pylint: auto fixes

--- Alpha/modules/helper_funcs/telethn/chatstatus.py
+++ Alpha/modules/helper_funcs/telethn/chatstatus.py
@@ -1,6 +1,7 @@
+from telethon.tl.types import ChannelParticipantsAdmins
+
 from Alpha import DRAGONS
 from Alpha.modules.helper_funcs.telethn import IMMUNE_USERS, telethn
-from telethon.tl.types import ChannelParticipantsAdmins
 
 
 async def user_is_ban_protected(user_id: int, message):
