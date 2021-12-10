74fc0b184be1a05d018ec19b3f9c98cd01cdaa14
Venuja104 <95410898+Venuja104@users.noreply.github.com>
2021-12-10 16:41:40 +0530

    Update remote_cmds.py

--- Alpha/modules/remote_cmds.py
+++ Alpha/modules/remote_cmds.py
@@ -1,12 +1,12 @@
-from Shizu import dispatcher
-from Shizu.modules.helper_funcs.chat_status import (
+from Alpha import dispatcher
+from Alpha.modules.helper_funcs.chat_status import (
     bot_admin,
     is_bot_admin,
     is_user_ban_protected,
     is_user_in_chat,
 )
-from Shizu.modules.helper_funcs.extraction import extract_user_and_text
-from Shizu.modules.helper_funcs.filters import CustomFilters
+from Alpha.modules.helper_funcs.extraction import extract_user_and_text
+from Alpha.modules.helper_funcs.filters import CustomFilters
 from telegram import ChatPermissions, Update
 from telegram.error import BadRequest
 from telegram.ext import CallbackContext, CommandHandler, run_async
