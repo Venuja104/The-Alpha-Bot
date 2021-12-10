6240e9f34b7214a845c06cab73f642cb2d0db23c
Venuja104 <95410898+Venuja104@users.noreply.github.com>
2021-12-10 16:10:04 +0530

    Update notes_sql.py

--- Alpha/modules/sql/notes_sql.py
+++ Alpha/modules/sql/notes_sql.py
@@ -1,8 +1,8 @@
 # Note: chat_id's are stored as strings because the int is too large to be stored in a PSQL database.
 import threading
 
-from Shizu.modules.helper_funcs.msg_types import Types
-from Shizu.modules.sql import BASE, SESSION
+from Alpha.modules.helper_funcs.msg_types import Types
+from Alpha.modules.sql import BASE, SESSION
 from sqlalchemy import Boolean, Column, Integer, String, UnicodeText, distinct, func
 
 
