0ddeb82f4b70ec2c6a74af80217c8bc36076d92c
Venuja104 <95410898+Venuja104@users.noreply.github.com>
2021-12-10 16:01:19 +0530

    Update msg_types.py

--- Alpha/modules/helper_funcs/msg_types.py
+++ Alpha/modules/helper_funcs/msg_types.py
@@ -1,6 +1,6 @@
 from enum import IntEnum, unique
 
-from Shizu.modules.helper_funcs.string_handling import button_markdown_parser
+from Alpha.modules.helper_funcs.string_handling import button_markdown_parser
 from telegram import Message
 
 
