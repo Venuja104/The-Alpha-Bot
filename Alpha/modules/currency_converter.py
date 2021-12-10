96c97800e17b345f582284d0e4b0887472348724
Venuja104 <95410898+Venuja104@users.noreply.github.com>
2021-12-10 16:27:43 +0530

    Update currency_converter.py

--- Alpha/modules/currency_converter.py
+++ Alpha/modules/currency_converter.py
@@ -1,5 +1,5 @@
 import requests
-from Shizu import CASH_API_KEY, dispatcher
+from Alpha import CASH_API_KEY, dispatcher
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, CommandHandler, run_async
 
