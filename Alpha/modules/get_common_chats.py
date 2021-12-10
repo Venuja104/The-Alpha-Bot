456b769b4aff0ab6eb5a993d0de68ebbbcfe7c51
Venuja104 <95410898+Venuja104@users.noreply.github.com>
2021-12-10 16:31:32 +0530

    Update get_common_chats.py

--- Alpha/modules/get_common_chats.py
+++ Alpha/modules/get_common_chats.py
@@ -1,9 +1,9 @@
 import os
 from time import sleep
 
-from Shizu import OWNER_ID, dispatcher
-from Shizu.modules.helper_funcs.extraction import extract_user
-from Shizu.modules.sql.users_sql import get_user_com_chats
+from Alpha import OWNER_ID, dispatcher
+from Alpha.modules.helper_funcs.extraction import extract_user
+from Alpha.modules.sql.users_sql import get_user_com_chats
 from telegram import Update
 from telegram.error import BadRequest, RetryAfter, Unauthorized
 from telegram.ext import CallbackContext, CommandHandler, Filters
