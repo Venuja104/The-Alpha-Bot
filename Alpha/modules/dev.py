f8fb369219e4e964faf45efd7aa781da36fa64f4
Venuja104 <95410898+Venuja104@users.noreply.github.com>
2021-12-10 16:29:29 +0530

    Update dev.py

--- Alpha/modules/dev.py
+++ Alpha/modules/dev.py
@@ -3,8 +3,8 @@ import subprocess
 import sys
 from time import sleep
 
-from Shizu import dispatcher
-from Shizu.modules.helper_funcs.chat_status import dev_plus
+from Alpha import dispatcher
+from Alpha.modules.helper_funcs.chat_status import dev_plus
 from telegram import TelegramError, Update
 from telegram.ext import CallbackContext, CommandHandler, run_async
 
